eb4193448bcf01c66149f320d9e4312895478b99
Ben <stealthton@gmail.com>
2023-02-12 14:28:47 +0000

    added some images and 3x2 grid with animations

---
+++ script.js
@@ -0,0 +1,27 @@
+// on scroll up, drop the header down - StackOverflow FTW !
+var position = $(window).scrollTop();
+
+// should start at 0
+
+$(window).scroll(function () {
+    var scroll = $(window).scrollTop();
+    console.log(scroll);
+    if (scroll >= 5) {
+        if (scroll > position) {
+            console.log('scrolling up');
+            // $('div').text('Scrolling Down Scripts');
+            $("#slide-header").slideUp("slow");
+        } else {
+            if (scroll < position + 5) {
+                console.log('scrolling down');
+                //  $('div').text('Scrolling Up Scripts');
+                // $("#slide-header").slideDown("slow");
+                $("#slide-header").delay("slow").fadeIn();    
+                //  $("#slide-header").css("position", "relative");
+            }
+        }
+    }
+
+    position = scroll;
+
+});
